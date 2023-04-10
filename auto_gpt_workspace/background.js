8779555485d145bc6e9a183c86eb4c19b4373064
kboogie23 <37029298+kboogie23@users.noreply.github.com>
2023-04-10 09:36:12 -0700

    test

--- auto_gpt_workspace/background.js
+++ auto_gpt_workspace/background.js
@@ -8,21 +8,23 @@ chrome.runtime.onInstalled.addListener(function() {
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) {
-    chrome.storage.local.get('isScanningOn', function(data) {
-      if (data.isScanningOn) {
-        if (
-          details.url.includes('outlook.com/api/v2.0/me/messages') &&
-          details.url.includes('$select=Subject') &&
-          (details.method === 'POST' || details.method === 'PATCH')
-        ) {
-          chrome.scripting.executeScript({
-            target: {tabId: details.tabId},
-            files: ['content_scripts.js']
-          });
+    return new Promise((resolve) => {
+      chrome.storage.local.get('isScanningOn', function(data) {
+        if (data.isScanningOn) {
+          if (
+            details.url.includes('outlook.com/api/v2.0/me/messages') &&
+            details.url.includes('$select=Subject') &&
+            (details.method === 'POST' || details.method === 'PATCH')
+          ) {
+            chrome.scripting.executeScript({
+              target: {tabId: details.tabId},
+              files: ['content_scripts.js']
+            });
+          }
         }
-      }
+        resolve({cancel: false});
+      });
     });
-    return {cancel: false};
   },
   {urls: ['<all_urls>']},
   ['blocking']
--- auto_gpt_workspace/improved_outlook_scanner.py
+++ auto_gpt_workspace/improved_outlook_scanner.py
@@ -11,7 +11,6 @@ def write_email_data_to_file(email_data_dict):
     with open('email_data.json', 'w') as f:
         json.dump(email_data_dict, f)
 
-
 class TestOutlookScanner(unittest.TestCase):
     def setUp(self):
         self.email_data_dict = {'some_email_data':'Example Data. From Junkmail.io'}
@@ -19,21 +18,16 @@ class TestOutlookScanner(unittest.TestCase):
         
     def test_scan_spam_email(self):
         # Spam email
-        self.email_data_dict['subject'] = 'Offer: Best deals on iPhone and iPad. Up to 50% '
-        'off @ iMart'
-        self.email_data_dict['body'] = 'Dear Com,
-' + 
-        'We are excited to offer our premium members exclusive discounts on some of the
-' + 
-        'hottest products from Apple. With savings of up to 50% off, there is no better time
-' + 
-        'to buy than now. Take advantage of this offer today and get the latest products
-' + 
-        'from Apple at unbeatable prices.
-' + 
-        'Sincerely,
-' + 
-        'iMart Team'
+        self.email_data_dict['subject'] = 'Offer: Best deals on iPhone and iPad. Up to 50% off @ iMart'
+        self.email_data_dict['body'] = (
+            'Dear Com,\n'
+            'We are excited to offer our premium members exclusive discounts on some of the\n'
+            'hottest products from Apple. With savings of up to 50% off, there is no better time\n'
+            'to buy than now. Take advantage of this offer today and get the latest products\n'
+            'from Apple at unbeatable prices.\n'
+            'Sincerely,\n'
+            'iMart Team'
+        )
         write_email_data_to_file(self.email_data_dict)
         result = outlook_scanner.scan_email(self.email_data_dict)
         self.assertEqual(result, True)
@@ -41,30 +35,23 @@ class TestOutlookScanner(unittest.TestCase):
     def test_scan_non_spam_email(self):
         # Non-spam email
         self.email_data_dict['subject'] = 'Wireless Association of Asia - Annual Convention'
-        self.email_data_dict['body'] = 'Dear members,
-' + 
-        'We are excited to invite you to the Wireless Association of Asia annual convention.
-' + 
-        'This year, the convention will be held in Tokyo, Japan, from July 1st - 3rd. The
-' + 
-        'convention provides an excellent opportunity to meet and network with peers,
-' + 
-        'gain an understanding of the latest trends and developments in the industry, and
-' + 
-        'exchange ideas about emerging issues. For more information about the convention
-' + 
-        'and to register, please visit our website: www.wirelessasia.com
-' + 
-        'Sincerely,
-' + 
-        'The Wireless Association of Asia'
+        self.email_data_dict['body'] = (
+            'Dear members,\n'
+            'We are excited to invite you to the Wireless Association of Asia annual convention.\n'
+            'This year, the convention will be held in Tokyo, Japan, from July 1st - 3rd. The\n'
+            'convention provides an excellent opportunity to meet and network with peers,\n'
+            'gain an understanding of the latest trends and developments in the industry, and\n'
+            'exchange ideas about emerging issues. For more information about the convention\n'
+            'and to register, please visit our website: www.wirelessasia.com\n'
+            'Sincerely,\n'
+            'The Wireless Association of Asia'
+        )
         write_email_data_to_file(self.email_data_dict)
         result = outlook_scanner.scan_email(self.email_data_dict)
         self.assertEqual(result, False)
         
     def tearDown(self):
         write_email_data_to_file({})
-
 
 if __name__ == '__main__':
     unittest.main() 
